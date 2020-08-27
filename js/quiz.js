2b3aff3d4dd43941f4b7f37c60c7b732e3e39fc4
Laurence Scotford <laurencescotford@gmail.com>
2020-08-27 06:13:58 +0100

    Add support for select all that apply type questions

--- js/quiz.js
+++ js/quiz.js
@@ -1,15 +1,35 @@
-function checkAnswer() {
+function checkAnswer(questionType, correct, numOptions) {
   let formValues = new URLSearchParams(window.location.search);
-  let result = 'incorrect!\nTry again or go to the next question';
-  let answer = formValues.get('answer');
   let correctFeedback = document.querySelector('#correct');
   let incorrectFeedback = document.querySelector('#incorrect');
   let nextQuestion = document.querySelector('#nextQuestion');
+  let answerGiven = document.querySelector('#answerGiven');
 
-  if (answer == '3') {
-    correctFeedback.classList.remove('hidden');
-    nextQuestion.classList.remove('hidden');
-  } else if (answer != null) {
-    incorrectFeedback.classList.remove('hidden');
+
+  if (!formValues.entries().next().done) {
+    let answer = "";
+    let answerString = "";
+    if (questionType == "multi") {
+      for (let i = 1; i <= numOptions; i = i + 1) {
+        if (formValues.has('answer' + i)) {
+          let commaString = answer.length > 0 ? ", " : "";
+          answer += commaString + i;
+          answerString += commaString + document.querySelector("#label" + i).innerHTML
+        }
+      }
+    } else {
+      answer = formValues.get('answer');
+      answerString = document.querySelector("#label" + answer).innerHTML;
+    }
+
+    answerGiven.innerHTML = "Your answer was " + answerString;
+    answerGiven.classList.remove('hidden');
+
+    if (answer == correct) {
+      correctFeedback.classList.remove('hidden');
+      nextQuestion.classList.remove('hidden');
+    } else {
+      incorrectFeedback.classList.remove('hidden');
+    }
   }
 }
